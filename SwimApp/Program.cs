43e4bc54325c2214e602490e7c29397126e960a2
Ellen Boyd <ellboyd@chbc.school.nz>
2023-02-13 12:27:55 +1300

    OneSwimmer() - Record swimmer details - calculate swimmer average - assign swimmer to a team

--- SwimApp/Program.cs
+++ SwimApp/Program.cs
@@ -1,12 +1,70 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace SwimApp
 {
     class Program
     {
+
+        //global variables
+        static List<string> teamA = new List<string>();
+        static List<string> teamB = new List<string>();
+        static List<string> teamReserves = new List<string>();
+
+        static void OneSwimmer()
+        {
+            Console.WriteLine("Enter the swimmer's name:");
+            
+            string swimmerName = Console.ReadLine();
+
+            Console.WriteLine($"Swimmer name: {swimmerName}");
+
+            int sumTotalTime = 0;
+            
+            //Loop 4 times
+            for (int i = 0; i < 4; i++)
+            {
+                int minutes, seconds, totalTime = 0;
+
+                //Generate a random number between 1 and 4 (incl)
+                Random randomNumber = new Random();
+                minutes = randomNumber.Next(1, 4);
+                seconds = randomNumber.Next(0, 59);
+                                
+                totalTime = (minutes * 60) + seconds;
+
+                Console.WriteLine($"Swimmer Time{i + 1}: {minutes} min {seconds} seconds\nTotal time in seconds: {totalTime}s\n");
+
+                sumTotalTime += totalTime;
+
+            }
+
+            float avgTime = (float)sumTotalTime / 4;
+
+            //assign the swimmer to a team
+            if (avgTime <= 160)
+            {
+                teamA.Add(swimmerName);
+            }
+            else if(avgTime <= 210)
+            {
+                teamB.Add(swimmerName);
+            }
+            else
+            {
+                teamReserves.Add(swimmerName);
+            }
+
+
+            Console.WriteLine($"Avg time: {avgTime}");
+
+
+
+        }
+
         static void Main(string[] args)
         {
-            
+            OneSwimmer();
         }
     }
 }
